5a0ce59d4f6b5fed6ef960096b1fc0f048eb775c
Kelvin Charles <kelvinc1291@hotmail.com>
2023-06-06 20:49:21 -0300

    Adicionado Modificações ao README.MD e identação de codigo

--- README.md
+++ README.md
@@ -1,11 +1,42 @@
-# Frontend Mentor | Advice generator app
+# 🖥️ Projeto Advice Generator APP Frontend Mentor
 
-Segundo projeto real que fiz baseados nos meus estudos!
+## Descrição
 
- <img src="./img-gif.gif" alt="imagem do projeto">
+Projeto inicialmente feito com HTML e CSS que eram meu conhecimento na época. Hoje foi atualizado com JavaScript e requisição de  API que estou estudando. Exercício proposto pelo curso DevQuest .
 
-## Tecnologias ultilizadas
-- HTML 5
-- CSS 3
+## ✍️ Processo
 
-### Futuramente irei atualizar com js para gerar varios conselhos!
+    - Marcação semântica HTML5
+    - Propriedades personalizadas de CSS3
+    - Flexbox
+    - Design responsivo
+    - JavaScript
+    - Apis
+
+## ⚙ Funcionalidades
+
+    - Gerar conselhos aleatórios ao clicar no botão
+     
+
+>Acesse a página on-line: <a href="https://frontend-mentor-advice-generator-app-xi.vercel.app/" target=_blank> Advice Generator App </a>
+
+
+## 🖱️ A página
+
+<img src="design/gif.gif" alt="Imagem exibindo a versão desktop  do site">
+
+## 👩‍💻 Dev
+
+<table align="center">
+    <tr>   
+        <td align="center">
+            <div>
+                <img src="https://avatars.githubusercontent.com/u/110488969?v=4" width="120px;" alt="Foto de Kelvin no GitHub"/><br>
+                <b> Kelvin Charles </b><br>
+                <a href="https://www.linkedin.com/in/kelvin-charles/" alt="Linkedin"><img src="https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white" height="20"></a>
+                <a href="https://github.com/kelvincharlesdev" alt="GitHub"><img src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white" height="20"></a>
+            </div>
+        </td>
+    </tr>
+</table>
+
--- src/js/index.js
+++ src/js/index.js
@@ -1,32 +1,29 @@
 // 1. Pegar o elemento de  **botão** para ser adicionado o **evento de clique** nele.
-// 2. Pegar o elemento de **id do conselho** 
+// 2. Pegar o elemento de **id do conselho**
 // 3. Pegar o elemento de **descrição do conselho**
 // 4. Após conseguirmos os elementos do html, é necessário a criação da **função responsável por pegar os dados da API**. A melhor forma de se consumir a api é usando **async await**. Caso não tenha visto a aula, sugiro que assista novamente. Nas referências ficará um link com exemplos.
 // 5. Dentro da função, após pegarmos os dados da API, é necessário atualizar o conteúdo do id e descrição do conselho que colocamos em uma variável usando o **innerHTML**.
 // 6. Por fim, basta chamar a função criada.
 // 7. Atribuir a função que criamos para ser executada no evento do clique do botão ou se preferir, criar o escopo da função dentro do listener.
 
-const btn = document.querySelector(".conselho-atualizacao")
+const btn = document.querySelector(".conselho-atualizacao");
 
-const id = document.querySelector(".conselho-titulo")
+const id = document.querySelector(".conselho-titulo");
 
-const conselhoAleatorio = document.querySelector(".conselho-descricao")
+const conselhoAleatorio = document.querySelector(".conselho-descricao");
 
+async function pegarConselho() {
+  const url = "https://api.adviceslip.com/advice";
+  const resposta = await fetch(url);
+  const json = await resposta.json();
+  const conselhoid = `Advice #${json.slip.id}`;
+  const conselhoDescricao = ` "${json.slip.advice}"`;
 
-async function pegarConselho () {
-    const url = "https://api.adviceslip.com/advice"
-    const resposta = await fetch(url)
-    const json =  await resposta.json()
-    const conselhoid = `Advice #${json.slip.id}`
-    const conselhoDescricao = ` "${json.slip.advice}"`
+  id.innerHTML = conselhoid;
 
-    id.innerHTML = conselhoid
-
-    conselhoAleatorio.innerHTML = conselhoDescricao
-
+  conselhoAleatorio.innerHTML = conselhoDescricao;
 }
 
 btn.addEventListener("click", () => {
-    pegarConselho()
-})
-
+  pegarConselho();
+});
